257784fc102287f5888484acd2b0a4a58457bd39
john Caropino <jccaropino7786@live.com>
2022-11-27 10:58:49 -0800

    commit tree 99709088d0cc381a55a923f4e0a3b5fd0e8b44c6

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+const drivers = ['Antonia','Nuru','Amari','Mo']
+
+function returnFirstTwoDrivers(){
+   return (drivers.slice(0,2))
+}
+
+function returnLastTwoDrivers(){
+    return (drivers.slice(2,length[-1]))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+// const createFareMultiplier= num => {
+//     const fareMultiplier = fare => num  * fare;
+//     return fareMultiplier;
+// }
+
+// return function createFare()
+
+function createFareMultiplier(num){
+    return function fareMultiplier(fare){
+        return fare * num;
+    }
+}
+function fareDoubler(fare){
+    return (fare * 2)
+}
+
+function fareTripler(fare){
+    return (fare *3)
+}
+    
+// function createFareMultiplier (num) {
+//     return function fareMultipler(fare) {
+//          return fare * num;
+//      } 
+//  }
+
+
+function selectDifferentDrivers(drivers,fn ){
+    return fn(drivers)
+}
+
+    
